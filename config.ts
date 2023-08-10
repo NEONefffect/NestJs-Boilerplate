a7123a7f494ab8e540fc846f893eb6a5aee04692
NEONeffect <antongntenko222@gmail.com>
2023-08-10 08:32:18 +0300

    dotenv

---
+++ config.ts
@@ -0,0 +1,28 @@
+import { config } from "dotenv";
+import * as path from "path";
+
+const root = path.join.bind(this, __dirname);
+config({ path: root("../") });
+
+export default {
+  PORT: process.env.PORT || 5000,
+  JWT_SECRET: process.env.JWT_SECRET,
+  ROOT_EMAIL: process.env.ROOT_EMAIL,
+  USER_PORTAL_HOST: process.env.USER_PORTAL_HOST,
+  SALT: process.env.SALT,
+  DB: {
+    port: +process.env.DB_PORT || 5432,
+    host: process.env.DB_HOST || "localhost",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  AWS: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_ACCESS_SECRET,
+    region: process.env.AWS_REGION,
+  },
+  S3_BUCKETS: {
+    S3_BUCKETS_CDN: process.env.S3_BUCKETS_CDN,
+  },
+};
